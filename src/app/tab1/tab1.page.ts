2705eade8ce746677ee5e8bd5d2bb025e2b3ba6c
Sören Gieth <soeren.gieth@fortis-it.de>
2025-01-12 20:02:40 +0100

    initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,10 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import {ZXingScannerModule} from "@zxing/ngx-scanner";
 import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject} from "rxjs";
+import { BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'app-tab1',
@@ -15,11 +17,59 @@ export class Tab1Page {
     private http: HttpClient,
   ) {}
 
+  availableDevices: MediaDeviceInfo[] = [];
+  deviceCurrent: MediaDeviceInfo | undefined;
+  deviceSelected: string = "";
 
+  formatsEnabled: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.QR_CODE,
+  ];
+  hasDevices: boolean = false;
+  hasPermission: boolean = false;
   qrResultString: string = "";
-  name: string = "";
+  torchEnabled = false;
+  torchAvailable$ = new BehaviorSubject<boolean>(false);
+  tryHarder = false;
+
+  name = ""
+  clearResult(): void {
+    this.qrResultString = "null";
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
+  }
+
+  onDeviceSelectChange(selected: string) {
+    const selectedStr = selected || '';
+    if (this.deviceSelected === selectedStr) { return; }
+    this.deviceSelected = selectedStr;
+    const device = this.availableDevices.find(x => x.deviceId === selected);
+    this.deviceCurrent = device || undefined;
+  }
+
+  onDeviceChange(device: MediaDeviceInfo) {
+    const selectedStr = device?.deviceId || '';
+    if (this.deviceSelected === selectedStr) { return; }
+    this.deviceSelected = selectedStr;
+    this.deviceCurrent = device || undefined;
+  }
+
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+  }
+
+  onTorchCompatible(isCompatible: boolean): void {
+    this.torchAvailable$.next(isCompatible || false);
+  }
 
   onCodeResult(resultString: string) {
+    console.log("invoked")
     this.qrResultString = resultString;
     this.http.get("https://world.openfoodfacts.org/api/v2/search?code=" + this.qrResultString)
       .subscribe(
@@ -31,7 +81,4 @@ export class Tab1Page {
         })
   }
 
-  clearResult(): void {
-    this.qrResultString = "";
-  }
 }
