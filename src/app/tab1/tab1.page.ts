d94161eb48228c52f56680592618912c7a1dee00
Sören Gieth <soeren.gieth@fortis-it.de>
2025-01-12 18:05:48 +0100

    initial commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {ZXingScannerModule} from "@zxing/ngx-scanner";
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, ZXingScannerModule],
+})
+export class Tab1Page {
+  constructor() {}
+}
