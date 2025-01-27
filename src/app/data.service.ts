8ec9122ab02371aa30bee71f43b3fdd1f356c493
Sören Gieth <soeren.gieth@fortis-it.de>
2025-01-27 19:35:02 +0100

    initial commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,33 @@
+import {Injectable, Signal, signal, WritableSignal} from '@angular/core';
+import { Product } from "./models/Product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private _currentScannedProduct: WritableSignal<Product | undefined> = signal(undefined)
+  private _addedProducts: WritableSignal<Product[]> = signal([])
+
+  constructor() {}
+
+  get currentScannedProduct(): Signal<Product | undefined> {
+    return this._currentScannedProduct;
+  }
+
+  set currentScannedProduct(value: Product | undefined) {
+    this._currentScannedProduct.set(value);
+  }
+
+  get addedItems(): Signal<Product[]> {
+    return this._addedProducts;
+  }
+
+  clearAddedItems(): void {
+    this._addedProducts.set([])
+  }
+
+  addItem(product: Product): void {
+    this._addedProducts.set([...this._addedProducts(), product])
+  }
+}
